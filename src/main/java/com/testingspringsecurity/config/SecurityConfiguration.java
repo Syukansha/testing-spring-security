32dd9b026d828e0c116fcf1cb07cefddf19149e6
syukriishak <syukriishak622@gmail.com>
2023-01-01 05:15:55 +0800

    update dependency

---
+++ src/main/java/com/testingspringsecurity/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.testingspringsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
